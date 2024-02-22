61e18394bc5c0e15fffea61bc13e8acdc29c1be4
Joan Filipe <joanfilipe0@gmail.com>
2024-02-22 14:57:15 -0300

    Aula 17 e 18

--- Aula 17/index.js
+++ Aula 17/index.js
@@ -1,12 +1,8 @@
 const { ApolloServer } = require("apollo-server");
 const graphql = require("./src/graphql");
-const UsuarioCadastroService = require("./src/services/UsuarioCadastroService");
 
 const server = new ApolloServer({
   ...graphql,
-  context: () => ({
-    usuarioCadastroService: UsuarioCadastroService,
-  }),
 });
 
 server.listen().then(({ url }) => console.log(url));
